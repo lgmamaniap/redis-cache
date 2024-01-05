e17e6b6e4e291c717c6cd9401ab734cb3c12f0f5
gustavo <lgmamaniap@gmail.com>
2024-01-05 11:03:21 -0400

    fix: corrección en conexión con redis

--- src/index.js
+++ src/index.js
@@ -8,16 +8,13 @@ const { promisify } = require('util')
 dotenv.config()
 
 const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD
 })
 
-const GET_ASYNC = promisify(client.get).bind(client)
-const SET_ASYNC = promisify(client.set).bind(client)
-
-// client.on('error', err => console.log('Redis Client Error', err))
-// client.connect()
+client.on('error', err => console.log('Redis Client Error', err))
+client.connect()
 
 
 const app = express()
@@ -25,20 +22,18 @@ app.use(responseTime())
 
 // Controladores
 app.get('/character', async (req, res) => {
-    await client.connect()
-    const reply = await client.get('characters')
-    if (reply) {
-        return res.json(JSON.parse(reply))
-    }
+  const reply = await client.get('characters')
+  if (reply) {
+    return res.json(JSON.parse(reply))
+  }
 
-    const response = await axios.get(`${process.env.API_URL}`)
+  const response = await axios.get(`${process.env.API_URL}`)
 
-    await client.set('characters', JSON.stringify(response.data))
+  await client.set('characters', JSON.stringify(response.data))
 
-    await client.disconnect()
-    return res.json(response.data)
+  return res.json(response.data)
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running')
+  console.log('Server is running')
 })
